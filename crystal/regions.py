c2220477c9d57bc99cd0845cc29bf614bcbbb6b3
Brent Pedersen <bpederse@gmail.com>
2014-09-10 18:42:33 -0600

    correctly handle regions

--- crystal/regions.py
+++ crystal/regions.py
@@ -14,13 +14,14 @@ def region_cluster_gen(features, regions_bed):
     regions are read into memory, but feature_gen is sorted.
 
     >>> from crystal import Feature
-    >>> feats = [Feature('chr1', i, []) for i in [1, 10, 20, 200, 1000, 1001]]
+    >>> feats = [Feature('chr1', i, []) for i in [1, 10, 20, 25, 200, 1000, 1001]]
     >>> with open('/tmp/zxzz.bed', 'w') as fh:
-    ...     fh.write('chr1\t5\t25\nchr1\t999\t1002\n')
+    ...     fh.write('chr1\t5\t24\nchr1\t25\t26\nchr1\t999\t1002\n')
 
     >>> for cluster in region_cluster_gen(feats, fh.name):
     ...    print cluster
     [Feature(chr1:10), Feature(chr1:20)]
+    [Feature(chr1:25)]
     [Feature(chr1:1000), Feature(chr1:1001)]
     """
 
@@ -29,25 +30,31 @@ def region_cluster_gen(features, regions_bed):
     for i, toks in enumerate(ts.reader(regions_bed, header=False)):
         # see if it's a header.
         if i == 0 and not (toks[1] + toks[2]).isdigit(): continue
-        regions[toks[0]].append((int(toks[1]), int(toks[2])))
+        regions[toks[0]].append((int(toks[1]), int(toks[2]), "-".join(toks)))
     regions = dict(regions)
+    # TODO: handle overlapping regions
 
     dmr = []
     for f in features:
         cr = regions.get(f.chrom)
         if cr is None:
             if dmr != []:
-                yield dmr
+                yield [x[0] for x in dmr]
                 dmr = []
             continue
 
-        if any(s <= f.position <= e for (s, e) in cr):
-            dmr.append(f)
-        elif dmr != []:
-                yield dmr
-                dmr = []
+        try:
+            reg = next(rid for (s, e, rid) in cr if s <= f.position <= e)
+        except StopIteration:
+            continue
 
-    if dmr != []: yield dmr
+        if dmr != [] and dmr[-1][-1] != reg:
+            yield [x[0] for x in dmr]
+            dmr = []
+
+        dmr.append((f, reg))
+
+    if dmr != []: yield [x[0] for x in dmr]
 
 if __name__ == "__main__":
     import doctest
