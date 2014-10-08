8761c5d43dd538f60af9fe79d4754e332aaf818a
Brent Pedersen <bpederse@gmail.com>
2014-10-08 14:59:25 -0600

    add evaluation data and update notebook

---
+++ notebooks/run-array-methods.R
@@ -0,0 +1,108 @@
+library(data.table)
+
+
+run.dmrcate = function(M, design, coef="genderM", outf){
+    library(DMRcate)
+    ilmn = readRDS('manifest.450.rds')
+    rownames(M) = ilmn[rownames(M), "IlmnID"]
+    myanno = cpg.annotate(M, analysis.type="differential", design=design, coef=coef, pcutoff=1)
+    dmrs = dmrcate(myanno, lambda=300, pcutoff=1)
+    df = dmrs$results
+    pos = df$hg19coord
+    chroms = unlist(lapply(strsplit(pos, ":", fixed=TRUE), function(r){ r[1] }))
+    starts = unlist(lapply(strsplit(pos, ":", fixed=TRUE), function(r){
+                     as.integer(unlist(strsplit(r[2], "-"))[[1]]) }))
+    ends =   unlist(lapply(strsplit(pos, ":", fixed=TRUE), function(r){
+                     as.integer(unlist(strsplit(r[2], "-"))[[2]]) }))
+
+    write.table(data.frame(chrom=chroms, start=starts-1, end=ends, p=df$meanpval, df),
+                quote=F, sep="\t", row.names=F, file=outf)
+}
+
+
+run.bumphunter = function(M, design, coef="genderM", outf){
+    library(bumphunter)
+    library(doParallel)
+
+    chroms = unlist(lapply(strsplit(rownames(M), ":", fixed=TRUE), function(v) v[[1]]))
+    posns = unlist(lapply(strsplit(rownames(M), ":", fixed=TRUE), function(v) as.integer(v[[2]])))
+
+    coef = grep(coef, colnames(design), fixed=TRUE)
+    if (!any(coef)){ stop(sprintf("%s not found in design", coef)) }
+    stopifnot(length(coef) == 1)
+
+    cl = clusterMaker(chroms, posns, maxGap=300)
+    registerDoParallel(cores=5)
+    tab = bumphunter(M, design, chroms, posns, cl, smooth=TRUE, B=400, cutoff=0.01, coef=coef)#pickCutoff=T)
+    colnames(tab$table)[1] = "chrom"
+    tab$table$p = tab$table$p.value # p.valueArea
+    write.table(tab$table, file=outf, quote=F, sep="\t", row.names=F)
+}
+
+run.limma = function(M, design, coef="genderM", outf){
+    library(limma)
+
+    fit = eBayes(lmFit(M, design), trend=FALSE)
+    top = topTable(fit, coef=coef, n=Inf, sort.by='none')
+
+    chroms = unlist(lapply(strsplit(as.character(rownames(top)), ":", fixed=TRUE), function(r){ r[1] }))
+    posns = unlist(lapply(strsplit(as.character(rownames(top)), ":", fixed=TRUE), function(r){ as.integer(r[2]) }))
+
+    write.table(data.frame(chrom=chroms, start=posns - 1, end=posns, p=top$P.Value, top), quote=F, sep="\t", row.names=F,
+                file=outf)
+}
+
+run.champ = function(M, design, coef, outf){
+    library(ChAMP)
+    M = 1 / (1 + exp(-M))
+    design = data.frame(design)
+    design$Sample_Group = design[,coef]
+    design$Sample_Name = rownames(design)
+    r = champ.lasso(beta.norm=M, pd=data.frame(design), filterXY=FALSE,
+                    image=FALSE, mafPol.upper=0, minDmrSep=300,
+                    minSigProbesLasso=1,  bedFile=TRUE,
+                    resultsDir=dirname(outf),
+                    adjPVal=0.2,
+                    DMRpval=1)
+    write.table(r, file=outf, quote=F, sep="\t", row.names=F)
+}
+
+
+run.betabinomial = function(M, design, coef, outf, mc.cores=5){
+    library(betareg)
+    library(parallel)
+    model = paste(grep("Intercept", colnames(design), invert=T, value=T), collapse=" + ")
+    model = sprintf("y ~ %s", model)
+    df = data.frame(design)
+
+    chroms = unlist(lapply(strsplit(as.character(rownames(M)), ":", fixed=TRUE), function(r){ r[1] }))
+    posns = unlist(lapply(strsplit(as.character(rownames(M)), ":", fixed=TRUE), function(r){ as.integer(r[2]) }))
+
+    M = 1 / (1 + exp(-M))
+
+    res = mclapply(1:nrow(M), function(i){
+        if(i %% 10000 == 0){ message(paste("at record", i)) }
+        df$y = M[i,]
+        r = summary(betareg(as.formula(model), df))$coefficients$mean[coef,]
+        data.frame(chrom=chroms[i], start=posns[i] - 1,
+                   end=posns[i], p=r[['Pr(>|z|)']])
+  }, mc.cores=mc.cores)
+  res = rbindlist(res)
+  res$qvalue = p.adjust(res$p, "fdr")
+  write.table(res, quote=F, row.names=F, sep="\t", file=outf)
+}
+
+args = commandArgs(TRUE)
+
+covs = read.csv(args[1])
+rownames(covs) = covs$id
+M = as.matrix(read.delim(args[2], row.names=1))#, nrow=2000))
+
+design = model.matrix(~ gender + age, covs)
+coef = "genderM"
+
+run.betabinomial(M, design, coef, outf='work/betabinomial.output.txt')
+run.dmrcate(M, design, coef, outf='work/dmrcate.output.txt')
+run.limma(M, design, coef, outf='work/limma.output.txt')
+run.bumphunter(M, design, coef, outf='work/bumphunter.output.txt')
+run.champ(M, design, coef, outf='work/champ.output.txt')
---
+++ scripts/eval-bed.py
@@ -0,0 +1,23 @@
+import sys
+import toolshed as ts
+from crystal.evaluate import evaluate
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+rbed = sys.argv[1]
+
+
+fig, ax = plt.subplots(1)
+
+for pbed in sys.argv[2:]:
+    bediter = [(d.get('chrom', d.get('chr')), int(float(d['start'])), int(float(d['end'])),
+                    float(d.get('p', d.get('z_p', d.get('slk_p')))), 1) for d in ts.reader(pbed)]
+
+
+    evaluate(bediter, rbed, ax=ax, label=pbed.rsplit(".", 1)[0].split("/", 1)[-1])
+
+
+ax.plot([0, 1], [0, 1], 'k--')
+ax.legend()
+plt.show()
+
